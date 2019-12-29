2b705eaa7d6aa6376097c6093e2be71d9f7b85af
zhy12345 <523200157@qq.com>
2019-12-29 19:54:26 +0800

    M add file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './stores'
 import 'normalize.css';
+import './index.less';
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
   , 
   document.getElementById('root')
 );
